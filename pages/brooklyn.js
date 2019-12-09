cbe115d9239fa2d4b0a2f5097569bd1fb303018d
azheng4119 <azheng4119@bths.edu>
2019-12-09 15:15:23 -0500

    toggle heatmap and airbnb

--- pages/brooklyn.js
+++ pages/brooklyn.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text,StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
 import MapView, { Heatmap, PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import axios from 'axios';
 
@@ -9,7 +9,9 @@ export default class Brooklyn extends React.Component {
         super(props);
         this.state = {
             coords: [],
-            issues: []
+            issues: [],
+            pins: false,
+            heat: false,
         }
     }
 
@@ -17,33 +19,33 @@ export default class Brooklyn extends React.Component {
         this._isMounted = true;
         try {
             let { data } = await axios.get('https://data-visual-api.herokuapp.com/borough/brooklyn');
-			let coords = [];
-			data.slice(0,200).forEach((e, i) => {
-				if (e.longitude && e.latitude) {
-					color = e.price <= 100 ? 'yellow' : (e.price <= 200 && e.price > 100) ? 'orange' : '#ff0000';
-					coords.push(
-						<Marker key={i}
-							coordinate={{ latitude: e.latitude, longitude: e.longitude }}
-							pinColor={color}>
-							<MapView.Callout >
-								<Text>Price: ${e.price} per night{"\n"}Number Of Reviews: {e.number_of_reviews}{"\n"}Room Type: {e.room_type}</Text>
-							</MapView.Callout>
-						</Marker>
-					)
-				}
-			});
+            let coords = [];
+            data.slice(0, 200).forEach((e, i) => {
+                if (e.longitude && e.latitude) {
+                    color = e.price <= 100 ? 'yellow' : (e.price <= 200 && e.price > 100) ? 'orange' : '#ff0000';
+                    coords.push(
+                        <Marker key={i}
+                            coordinate={{ latitude: e.latitude, longitude: e.longitude }}
+                            pinColor={color}>
+                            <MapView.Callout >
+                                <Text>Price: ${e.price} per night{"\n"}Number Of Reviews: {e.number_of_reviews}{"\n"}Room Type: {e.room_type}</Text>
+                            </MapView.Callout>
+                        </Marker>
+                    )
+                }
+            });
             if (this._isMounted) {
                 this.setState({
                     coords: coords,
                 })
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
-        try{
+        try {
             let { data } = await axios.get('http://data-visual-api.herokuapp.com/service/brooklyn');
-			let issues = [];
+            let issues = [];
             data.forEach((e, i) => {
                 if (e.longitude && e.latitude) {
                     issues.push({
@@ -56,74 +58,88 @@ export default class Brooklyn extends React.Component {
             });
             if (this._isMounted) {
                 this.setState({
-                    issues:issues
+                    issues: issues
                 })
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
     }
 
-	componentWillUnmount() {
-		this._isMounted = false;
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
-	onMapReady = () => this.setState({ marginBottom: 0 })
+    onMapReady = () => this.setState({ marginBottom: 0 })
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<MapView 
-					provider={PROVIDER_GOOGLE}
-					onMapReady={this.onMapReady}
-					style={[styles.map, { flex: 1, marginBottom: this.state.marginBottom }]}
-					ref={ref => { this.mapView = ref }}
-					initialRegion={{
-						latitude: 40.644823,
-						longitude: -73.958926,
-						latitudeDelta: 0.16,
-						longitudeDelta: 0.16
-					}}
-					showsCompass={false}
-					loadingEnabled={true}>
-					{this.state.coords}
-                    <Heatmap
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.icon} onPress={()=>this.setState({pins : !this.state.pins})}><Text>Airbnb</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.icon} onPress={()=>this.setState({heat : !this.state.heat})}><Text>311</Text></TouchableOpacity>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    onMapReady={this.onMapReady}
+                    style={[styles.map, { flex: 1, marginBottom: this.state.marginBottom }]}
+                    ref={ref => { this.mapView = ref }}
+                    initialRegion={{
+                        latitude: 40.644823,
+                        longitude: -73.958926,
+                        latitudeDelta: 0.16,
+                        longitudeDelta: 0.16
+                    }}
+                    showsCompass={false}
+                    loadingEnabled={true}>
+                    {this.state.pins && this.state.coords}
+                    {this.state.heat && <Heatmap
                         points={this.state.issues}
                         onZoomRadiusChange={{
                             zoom: [0, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17],
                             radius: [10, 10, 15, 20, 30, 60, 80, 100, 120, 150, 180, 200, 250, 250]
                         }}
                         gradient={{
-                            colors : ["#79BC6A", "#BBCF4C", "#EEC20B", "#F29305", "#E50000"],
-                            startPoints : [.0002,.005,.01,.03,.04],
-                            colorMapSize : 256
+                            colors: ["#79BC6A", "#BBCF4C", "#EEC20B", "#F29305", "#E50000"],
+                            startPoints: [.0002, .005, .01, .03, .04],
+                            colorMapSize: 256
                         }}
-                        opacity = {.7}
+                        opacity={.7}
                         radius={60}
                         heatmapMode="POINTS_DENSITY"
-                    ></Heatmap>
-				</MapView>
-			</View>
-		);
-	}
+                    ></Heatmap>}
+                </MapView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		bottom: 0,
-		right: 0,
-		justifyContent: 'flex-end',
-		alignItems: 'center'
-	},
-	map: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		bottom: 0,
-		right: 0
-	}
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        zIndex: -1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+    },
+    icon: {
+        alignSelf: 'flex-end', display: 'flex', justifyContent: 'center', alignItems: 'center', shadowColor: "#000000",
+        shadowOpacity: 0.4, shadowRadius: 2,
+        shadowOffset: {
+            height: 1,
+            width: 1
+        },
+        backgroundColor: 'white',
+        zIndex: 1,
+        margin: 10, width: 55, height: 55, borderRadius: 30
+    }
 });
